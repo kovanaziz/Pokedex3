07ada3f041876eea056ec285b23dfc0878cbad80
kovan <kovan.aziz08@gmail.com>
2016-09-17 19:21:13 +0300

    IBoutlet of pokemonDetailView added

--- Pokedex3/PokemonDetailVC.swift
+++ Pokedex3/PokemonDetailVC.swift
@@ -11,7 +11,21 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     var pokemon : Pokemon!
+    
+    
+    @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptoinLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAtackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var backLbl: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +34,8 @@ class PokemonDetailVC: UIViewController {
 
     }
 
+    @IBAction func backBtnPressed(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
